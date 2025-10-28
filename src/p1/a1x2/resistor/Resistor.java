b6acdd8b9cecc26e32712d3e333fd7e76424f9c6
Maksym Zadoya <zadoyamaxim@gmail.com>
2025-10-28 23:08:37 +0100

    Add some Javadoc comments

--- src/p1/a1x2/resistor/Resistor.java
+++ src/p1/a1x2/resistor/Resistor.java
@@ -1,5 +1,15 @@
 package p1.a1x2.resistor;
 
+/**
+ * Eine abstrakte Elternklasse fuer alle einfachen Widerstaende
+ * 
+ * Speicher gemeinsame Eigenschaften wie Name und Widerstandswert und bietet 
+ * eine gemeinsame Grundlage fuer OrdinaryResistor und Potentiometer.
+ * 
+ * @author Maksym Zadoya
+ * @version 2025/10/28 #1
+ *  
+ */
 public abstract class Resistor implements ResistanceNet {
 
    protected String name;
@@ -8,5 +18,6 @@ public abstract class Resistor implements ResistanceNet {
    public Resistor (String name, double value) {
        this.name = name;
        this.wiederstandwert = value;
+       
    }
 }
